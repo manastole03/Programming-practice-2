2e2364c7761252294c8ebeea41b787041f711bb4
shreyaschavhan <68887544+shreyaschavhan@users.noreply.github.com>
2020-07-28 21:36:05 +0530

    Add files via upload

---
+++ python2/day1_problem_statement1.py
@@ -0,0 +1,20 @@
+# Write a program to print the following string in a specific format (see the output).
+# Sample string:
+# "Twinkle, twinkle, little star, How I wonder what you are! Up above the world
+# so high, like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are!"
+# Expected Output:
+# Twinkle, twinkle, little star,
+#       How I wonder what you are!
+#               Up above the world so high,
+#               Like a diamond in the sky.
+# Twinkle, twinkle, little star,
+#       How I wonder what you are.
+
+print("""
+ Twinkle, twinkle, little star,
+       How I wonder what you are!
+               Up above the world so high,
+               Like a diamond in the sky.
+ Twinkle, twinkle, little star,
+       How I wonder what you are.
+""")
---
+++ python2/day1_program_statement5.py
@@ -0,0 +1,17 @@
+# Write a program to swap two variables.
+# i)using three variable.
+
+var1 = 8
+var2 = 4
+temp = None
+
+print("Before Swapping: ")
+print("var1 : ", var1, "and var2 : ", var2)
+temp = var1
+var1 = var2
+var2 = temp
+
+print("After Swapping: ")
+print("var1 : ", var1, "and var2 : ", var2)
+
+# ii. using 2 variables
