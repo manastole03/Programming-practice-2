071a36f8db1e9606e8b13b9ce546f2687bbf9d3b
shreyaschavhan <68887544+shreyaschavhan@users.noreply.github.com>
2020-07-28 22:00:04 +0530

    Update day1_program_statement5.py

--- python2/day1_program_statement5.py
+++ python2/day1_program_statement5.py
@@ -11,7 +11,15 @@ temp = var1
 var1 = var2
 var2 = temp
 
-print("After Swapping: ")
+print("[three variables: ]After Swapping: ")
 print("var1 : ", var1, "and var2 : ", var2)
 
 # ii. using 2 variables
+# Hint : The idea is to get sum in one of the two given numbers.
+# The numbers can then be swapped using the sum and subtraction from sum.
+var1 = var1 + var2
+var2 = var1 - var2
+var1 = var1 - var2
+print()
+print("[two variables: ]After Swapping: ")
+print("var1 : ", var1, "and var2 : ", var2)
