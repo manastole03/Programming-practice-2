9fae4437ad097932e7f02c187a86ab955c6cda4d
AtharvaD11 <67207001+AtharvaD11@users.noreply.github.com>
2020-08-01 09:06:20 +0530

    Delete Name.py

--- python1/Name.py
+++
@@ -1,4 +0,0 @@
-# Program which accepts the user's first and last name and print them in reverse order with a space between them.
-name = input("Please enter your name: ").split()
-a = name[::-1]
-print(*a,sep = " ")
