cdf67be8db468ce00cd8e48de054ad6f78729952
Mahesh P Bandewar <68895536+Maheshpb411@users.noreply.github.com>
2020-08-01 14:25:30 +0530

    Create Ask user to enter age, sex ( M or F ), marital status ( Y or N ) and then using following rules print their place of service. if employee is female, then she will work only in urban areas.py

---
+++ python 3/Ask user to enter age, sex ( M or F ), marital status ( Y or N ) and then using following rules print their place of service. if employee is female, then she will work only in urban areas.py
@@ -0,0 +1,13 @@
+n=int(input("enter a age"))
+print("enter gender of the employee")
+input("male")
+input("female")
+gender=input("")
+if(gender==f):
+    print("urban areas")
+elif(gender==m and n>20 and n<40):
+    print("anywhere")
+elif(gender==m and n>40 and n<60):
+    print("urban areas only")
+else:
+    print("error")
