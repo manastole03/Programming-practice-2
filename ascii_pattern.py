9f2e2f5aab75bec50adb24bbd930aeb0e62b5a4e
AtharvaD11 <67207001+AtharvaD11@users.noreply.github.com>
2020-08-04 08:16:21 +0530

    Delete ascii_pattern.py

--- ascii_pattern.py
+++
@@ -1,9 +0,0 @@
-
-# Python program to print Ascii Pattern
-asciiNumber = 65
-for i in range(0, 7):
-    for j in range(0, i + 1):
-        character = chr(asciiNumber)
-        print(character, end=' ')
-        asciiNumber += 1
-    print(" ")
