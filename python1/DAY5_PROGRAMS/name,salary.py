0eaff27ff68974fa858650b86864f7452fb7f283
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-08-04 22:59:31 +0530

    Update name,salary.py

--- python1/DAY5_PROGRAMS/name,salary.py
+++ python1/DAY5_PROGRAMS/name,salary.py
@@ -1,3 +1,7 @@
 def showEmployee(name,salary=9000):
     print("Name of employee:  {}".format(name))
     print("Salary of employee:  {}".format(salary))
+
+name = input("Enter your name:  ")
+salary = int(input("Enter salary:  "))
+showEmployee(name,salary)
