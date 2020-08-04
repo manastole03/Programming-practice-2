fed2e77ea775cc4a23e47274f7e3602d8f16d91b
AtharvaD11 <67207001+AtharvaD11@users.noreply.github.com>
2020-08-04 23:17:42 +0530

    Delete name,salary.py

--- python1/DAY5_PROGRAMS/name,salary.py
+++
@@ -1,7 +0,0 @@
-def showEmployee(name,salary=9000):
-    print("Name of employee:  {}".format(name))
-    print("Salary of employee:  {}".format(salary))
-
-name = input("Enter your name:  ")
-salary = int(input("Enter salary:  "))
-showEmployee(name,salary)
