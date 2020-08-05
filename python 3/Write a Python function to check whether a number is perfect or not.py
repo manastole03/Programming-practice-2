ea13a4114447e518707f12dec0bc71b10ee21e2f
Mahesh P Bandewar <68895536+Maheshpb411@users.noreply.github.com>
2020-08-05 20:15:13 +0530

    Create Write a Python function to check whether a number is perfect or not.py

---
+++ python 3/Write a Python function to check whether a number is perfect or not.py
@@ -0,0 +1,11 @@
+n=int(input("enter a number"))
+s=0
+for i in range(1,n):
+    if(n%i==0):
+        s+=i
+        
+if(s==n):
+    print(n,"is a perfect number")
+    
+else:
+    print(n,"is not perfect number")
