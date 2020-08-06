b642c5242fb57b945101f3b2932f09e3de7aa51d
Mahesh P Bandewar <68895536+Maheshpb411@users.noreply.github.com>
2020-08-06 22:17:07 +0530

    Create  Write a program that asks the user to enter two points on a 2D plane (i.e. enter X1 & Y1,enter X2 & Y2). Compute the distance between those points using a function.py

---
+++ python 3/Write a program that asks the user to enter two points on a 2D plane (i.e. enter X1 & Y1,enter X2 & Y2). Compute the distance between those points using a function.py
@@ -0,0 +1,11 @@
+def distance(a,b,c,d):
+    d=(((c-a)*2)+((d-b)*2))**(1/2)
+    print("distance between these two points is",d)
+
+    
+e=int(input("enter a number:"))  
+f=int(input("enter a number:"))  
+g=int(input("enter a number:"))  
+h=int(input("enter a number:"))
+
+distance(e,f,g,h)
