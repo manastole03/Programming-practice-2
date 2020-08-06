1bc2d559cbea3a4376d4743ef3abae01200d8883
Mahesh P Bandewar <68895536+Maheshpb411@users.noreply.github.com>
2020-08-06 22:13:05 +0530

    Create  Write a function that converts feet to inches. It should accept a number of feet as an argument and return the equivalent number of inches. Similarly for miles to kilometer, acre to square yards..py

---
+++ python 3/Write a function that converts feet to inches. It should accept a number of feet as an argument and return the equivalent number of inches. Similarly for miles to kilometer, acre to square yards..py
@@ -0,0 +1,28 @@
+n=int(input("enter a number in feet:"))
+
+def feet(x):
+    inch=n*12
+    print(n,"foot=",inch,"inches") 
+    
+feet(n)    
+
+    
+    
+    
+n=int(input("enter a number in miles:"))
+
+def miles(x):
+    km=n*1.609
+    print(n,"miles=",km,"km") 
+    
+miles(n)    
+
+
+
+n=int(input("enter a number in acre:"))
+
+def acre(x):
+    squareyard=n*4840
+    print(n,"acres=",squareyard,"square yard") 
+    
+acre(n)    
