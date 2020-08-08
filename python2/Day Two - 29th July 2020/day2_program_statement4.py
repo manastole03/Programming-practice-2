52ee66a65cd2ce1ba3ac0fe2f6cf955af414ec4e
shreyaschavhan <68887544+shreyaschavhan@users.noreply.github.com>
2020-08-08 15:08:40 +0530

    Delete day2_program_statement4.py

--- python2/Day Two - 29th July 2020/day2_program_statement4.py
+++
@@ -1,7 +0,0 @@
-# Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
-# (for eg: entered number is 5, output=5+55+555=615)
-
-number = int(input("Enter a number(n): "))
-nn = int("%i%i" % (number, number))
-nnn = int("%i%i%i" % (number, number, number))
-print("The value of n + nn + nnn: ", number + nn + nnn )
