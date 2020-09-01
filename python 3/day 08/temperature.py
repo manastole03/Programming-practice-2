805b2be37cecd1450e0c19ab7c4b82fc39639f2e
Mahesh P Bandewar <68895536+Maheshpb411@users.noreply.github.com>
2020-09-01 09:20:25 +0530

    Create temperature.py

---
+++ python 3/day 08/temperature.py
@@ -0,0 +1,17 @@
+#conversion of c to f or f to c.py
+class temperature:
+    def __init__(self,c,f):
+        self.c = c
+        self.f = f
+    def convertintoF(self):
+        print("farenhait scale: ",(self.c - 32) * (5/9))
+    def convertintoC(self):
+        print("celcius scale: ",((9/5) *self.f + 32)
+x  = float(input("enter a number in celcius : "))   
+y  = float(input("enter a number in farenhait : "))  
+
+t1 = temperature(x,y)
+t2 = temperature(x,y)
+
+t1.convertintoF()
+t2.convertintoC()
