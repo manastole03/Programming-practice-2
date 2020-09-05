9f7a22c2950cf3ddfdf1b0ec9ff8c8ffb7d9b1a9
Mahesh P Bandewar <68895536+Maheshpb411@users.noreply.github.com>
2020-09-05 19:04:19 +0530

    Update employee salary.py

--- python 3/day 09/employee salary.py
+++ python 3/day 09/employee salary.py
@@ -3,21 +3,21 @@ class employee:
         self.salary = salary
         self.hours_of_work = hours_of_work
     def based_on_salary(self):
-        if(self.salary < 5000):
+        if(self.salary < 5000 and self.hours_of_work < 6 ):
             self.salary = self.salary + 500
-            print(self.salary)
-        else:
-            print("no increment is there.")            
-    def based_on_hours(self):
-        if(self.hours_of_work >6):
+            print("your salary is",self.salary)
+            print("your work is ",self.hours_of_work)
+                
+        elif(self.hours_of_work >= 6 and self.salary < 5000 ):
             self.salary = self.salary +750
-            print(self.salary)
+            print("your salary is",self.salary)
+            print("your work is ",self.hours_of_work)
         else:
-            print("no increment is there.")
+            print(self.salary,"rs")
+            print(self.hours_of_work,"hours")
             
 s = int(input("enter the salary : ")) 
 h = int(input("enter the hours of work per day : "))
 
 p = employee(s,h)
 p.based_on_salary()
-p.based_on_hours()
