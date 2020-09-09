0dc4e6e066dd2fea9b43199a4da5d8dab460a2ae
AtharvaD11 <67207001+AtharvaD11@users.noreply.github.com>
2020-09-09 22:58:25 +0530

    Add files via upload

---
+++ python1/DAY12_PROGRAMS/typeerror.py
@@ -0,0 +1,6 @@
+choice = input("enter v or g ")
+
+if (choice == "v") or (choice == "g"):
+    pass
+else :
+   print("Not a valid choice! Try again")
---
+++ python1/DAY12_PROGRAMS/zerodivisionerror.py
@@ -0,0 +1,6 @@
+try:
+    a = 12
+    b = 0
+    c = a/b
+except ZeroDivisionError:
+    print("Cannot divide by 0")
